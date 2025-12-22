40f9e151778c6769be6bb54f95d09c8be14fbdb6
Aiden Maas <ajm.142020@gmail.com>
2025-12-22 18:39:02 -0500

    I finished the project

--- 2-advanced_f_strings.py
+++ 2-advanced_f_strings.py
@@ -6,8 +6,10 @@ def main():
     # 1. Collect 4 pieces of information
     name = input("Enter a name: ")
     place = input("Enter a place (e.g., Mars, The Kitchen): ")
-    # YOUR CODE HERE: Ask for an 'object'
+    # YOUR CODE HERE: Ask for an 'object'adjective = input("Enter a adjective :")
+    adjective =input("Enter a adjective :" )
     # YOUR CODE HERE: Ask for an 'adjective'
+    object = input("Enter a object :")
 
     # 2. Build the story
     # Hint: You can put a \n inside your string to start a new line!
@@ -16,7 +18,7 @@ def main():
     # YOUR CODE HERE:
     # Print a story like: "Once upon a time, [name] went to [place].
     # They found a [adjective] [object]. It was the best [object] ever!"
-
+    print(f"Once upon a time {name} and found an {adjective} {object} and it took him to {place}")
 
 if __name__ == "__main__":
     main()
