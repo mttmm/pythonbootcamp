c26afec10688424889720871afd136e7905a2a69
Aiden Maas <ajm.142020@gmail.com>
2025-12-23 08:29:58 -0500

    Started working on second part

--- Choose_your_adventure.py
+++ Choose_your_adventure.py
@@ -5,7 +5,11 @@
 name = input("What is your name? ")
 start_story = input("Do you want to start the story? (yes/no) ")
 if start_story == "yes":
-    print(f"once upon a time on a stormy night there was a man named {name}") 
-outside = input("do you want to go outside? yes/no ") 
-if outside == "yes"
-print("")
+    print(f"Once upon a time on a stormy night there was a man named {name}") 
+outside = input("Do you want to go outside? (yes/no) ") 
+if outside == "yes":
+     print("You go outside and feel the rain on your head it hits you hard and all the sudden it starts to hail")
+stay = input("Do you stay outside? (yes/no) ")
+if stay == "yes":
+    print("The hail hits you harder and harder you run and all the sudden you find a cave" )
+inside = input("Do you go in the cave? (yes/no) ")
