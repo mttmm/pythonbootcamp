bff882ec7f3bbec6493d0bae9a4611131eac4f71
Aiden Maas <ajm.142020@gmail.com>
2025-12-22 22:05:19 -0500

    I am making a choose your adventure

---
+++ Choose_your_adventure.py
@@ -0,0 +1,11 @@
+# get the user's name
+# print a greeting message
+# ask if you want to start the story 
+# Start the story
+name = input("What is your name? ")
+start_story = input("Do you want to start the story? (yes/no) ")
+if start_story == "yes":
+    print(f"once upon a time on a stormy night there was a man named {name}") 
+outside = input("do you want to go outside? yes/no ") 
+if outside == "yes"
+print("")
